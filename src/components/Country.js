a5a431651dfaad27f166bf088fb38b64e1632111
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-01 22:41:02 +0000

    added components

---
+++ src/components/Country.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Country extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default Country;
---
+++ src/components/CountryFilter.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class CountryFilter extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default CountryFilter;
