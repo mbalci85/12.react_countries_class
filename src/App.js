17b233c3aa11c356a85e5193be75d1cf7d9097e4
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-03-02 12:44:33 +0000

    added select function to filter by dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import Countries from './components/Countries';
+import CountryFilter from './components/CountryFilter';
 import allCountries from './countriesAll.json';
 
 export class App extends Component {
 	state = {
 		countries: allCountries,
+	};
+
+	handleSelect = (e) => {
+		this.setState({
+			countries:
+				e.target.value === 'selectRegion'
+					? allCountries
+					: allCountries.filter(
+							(country) => country.region === e.target.value,
+					  ),
+		});
 	};
 	render() {
 		return (
@@ -16,6 +28,7 @@ export class App extends Component {
 				}}
 			>
 				<h1>COUNTRIES</h1>
+				<CountryFilter handleSelect={this.handleSelect} />
 				<Countries countries={this.state.countries} />
 			</div>
 		);
--- src/components/CountryFilter.js
+++ src/components/CountryFilter.js
@@ -4,7 +4,24 @@ export class CountryFilter extends Component {
 	render() {
 		return (
 			<div>
-				<input />
+				<form>
+					<input
+						style={{ width: 300, margin: 8, height: 30 }}
+						placeholder="Type to Filter..."
+					/>
+					<select
+						style={{ width: 300, height: 30 }}
+						onChange={this.props.handleSelect}
+					>
+						<option value="selectRegion">Select Your Region</option>
+						<option value="Africa">Africa</option>
+						<option value="Americas">Americas</option>
+						<option value="Asia">Asia</option>
+						<option value="Europe">Europe</option>
+						<option value="Oceania">Oceania</option>
+						<option value="Polar">Polar</option>
+					</select>
+				</form>
 			</div>
 		);
 	}
